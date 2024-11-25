e0445d886f0420b690c4f7c14ac441b92b61cd2f
sanagikyota <m245813@hiroshima-u.ac.jp>
2024-11-25 21:22:22 +0900

    fig

--- plot_anal_398_402_241125.py
+++ plot_anal_398_402_241125.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 # データ
-values = [98.21428571428571, 83.03571428571429, 100.0, 95.45454545454545]
+values = [93.33333333333333, 75.0, 99.36708860759494, 100.0]
 labels = ['sk402_0.05mM', 'sk402_0.1mM', 'sk402', 'sk398']
 
 # 棒グラフの作成
@@ -9,10 +9,10 @@ plt.figure(figsize=(10, 6))
 plt.bar(labels, values, color='blue', width=0.4)
 #plt.xlabel('Sample')
 plt.ylabel('Death rate (%)')
-plt.title('Death rate 240713')
+plt.title('Death rate 240726')
 plt.ylim(0, 100)
 #plt.grid(axis='y', linestyle='-', linewidth=0.5)
 plt.gca().yaxis.grid(False)
 # グラフの保存
-plt.savefig("fig_bta_398402_240724", dpi=300)
+plt.savefig("fig_bta_398402_240726", dpi=300)
 plt.show()
